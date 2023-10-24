0491a9a67d301f1f6ecb646dd634b07d3c176031
Stullexy <66529400+SashaKilin@users.noreply.github.com>
2023-10-24 19:08:26 +0300

    open code

---
+++ Middle School Mark/Middle School Mark/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+
+namespace Middle_School_Mark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Marks = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Marks
+            // 
+            resources.ApplyResources(this.Marks, "Marks");
+            this.Marks.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Marks.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Marks.Name = "Marks";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Marks);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Marks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Middle School Mark/Middle School Mark/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Middle_School_Mark
+{
+    public partial class Form1 : Form
+    {
+
+        List<byte> mark = new List<byte>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mark.Add(5);
+            Marks.Text = Marks.Text + "5, ";
+            float pluss = 0;
+            
+            for (int i = 0; i< mark.Count; i++)
+            {
+                pluss = pluss + mark[i];
+            }
+
+            label2.Text = Convert.ToString(Math.Round(pluss / mark.Count, 2));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mark.Add(4);
+            Marks.Text = Marks.Text + "4, ";
+            float pluss = 0;
+
+            for (int i = 0; i < mark.Count; i++)
+            {
+                pluss = pluss + mark[i];
+            }
+
+            label2.Text = Convert.ToString(Math.Round(pluss / mark.Count, 2));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mark.Add(3);
+            Marks.Text = Marks.Text + "3, ";
+            float pluss = 0;
+
+            for (int i = 0; i < mark.Count; i++)
+            {
+                pluss = pluss + mark[i];
+            }
+
+            label2.Text = Convert.ToString(Math.Round(pluss / mark.Count, 2));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mark.Add(2);
+            Marks.Text = Marks.Text + "2, ";
+            float pluss = 0;
+
+            for (int i = 0; i < mark.Count; i++)
+            {
+                pluss = pluss + mark[i];
+            }
+
+            label2.Text = Convert.ToString(Math.Round(pluss / mark.Count, 2));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            mark.Clear();
+            Marks.Text = "";
+            label2.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (Marks.Text != "")
+            {
+                Marks.Text = Marks.Text.Remove(Marks.Text.Length - 3);
+
+
+                mark.Remove(mark.Last());
+
+                float pluss = 0;
+
+                for (int i = 0; i < mark.Count; i++)
+                {
+                    pluss = pluss + mark[i];
+                }
+
+                label2.Text = Convert.ToString(Math.Round(pluss / mark.Count, 2));
+
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '5')
+            {
+                button1.PerformClick();
+            }
+            else if (e.KeyChar == '4')
+            {
+                button2.PerformClick();
+            }
+            else if (e.KeyChar == '3')
+            {
+                button3.PerformClick();
+            }
+            else if (e.KeyChar == '2')
+            {
+                button4.PerformClick();
+            }
+            else if(e.KeyChar == (char)Keys.Back)
+            {
+                button6.PerformClick();
+            }
+        }
+    }
+}
